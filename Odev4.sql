86c889d945006f991356f61939a1e341fd10dc2f
Can Arik <canarik75@gmail.com>
2022-07-28 00:13:09 +0300

    V1.0

---
+++ Odev4.sql
@@ -0,0 +1,8 @@
+--Soru 1:
+SELECT country FROM country WHERE country LIKE 'A%a';
+--Soru2:
+SELECT country FROM country WHERE country LIKE '_____%n';
+--Soru 3:
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t%';
+--Soru 4:
+SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
