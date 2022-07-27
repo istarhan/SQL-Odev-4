b8f9118c0c4c1eb6b9a8e51f663d5640a24a78ff
Can Arik <canarik75@gmail.com>
2022-07-28 00:21:43 +0300

    V3.0

--- Odev4.sql
+++ Odev4.sql
@@ -1,9 +1,10 @@
 --Soru 1:
-
+SELECT DISTINCT replacement_cost FROM film;
 --Soru2:
-
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
 --Soru 3:
-
+SELECT COUNT(title) FROM film WHERE title LIKE 'T%' AND rating = 'G';
 --Soru 4:
-
+SELECT COUNT(country) FROM country WHERE country LIKE '_____';
 --Soru 5
+SELECT city FROM city WHERE city ILIKE '%R';
